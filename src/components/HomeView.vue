2ecea601063c41df6c9745c1a72f58530b34c7ca
Kaine Bismarck <kaine.bismarck@suprbiz.io>
2022-11-19 23:16:01 +0100

    main vue

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,3 +1,5 @@
 <template>
     <img class="mx-auto" alt="Vue logo" src="../assets/boilerplate.png" />
+
+    <!-- <button class="h-12 bg-red-300 px-[80px] max-w-max">Home Button</button> -->
 </template>
